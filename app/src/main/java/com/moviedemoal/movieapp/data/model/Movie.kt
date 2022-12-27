ce0a0b45359b8dd3506b97c2591ec3ee709582f6
AlvaroErd <alerdoci@gmail.com>
2022-12-27 13:53:34 +0100

    Agregada la entidad MovieEntity, preparando el Room

--- app/src/main/java/com/moviedemoal/movieapp/data/model/Movie.kt
+++ app/src/main/java/com/moviedemoal/movieapp/data/model/Movie.kt
@@ -1,5 +1,8 @@
 package com.moviedemoal.movieapp.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 data class Movie(
@@ -20,4 +23,36 @@ data class Movie(
     val vote_count: Int = -1
 )
 
-data class MovieList ( val results: List<Movie> = listOf())
+//Room
+//Hemos borrado el array
+@Entity
+data class MovieEntity(
+    @PrimaryKey
+    val id: Int = -1,
+    @ColumnInfo(name = "adult") //mismo nombre que la propiedad que define
+    val adult: Boolean = false,
+    @ColumnInfo(name = "backdrop_path")
+    val backdrop_path: String = "",
+    @ColumnInfo(name = "original_language")
+    val original_language: String = "",
+    @ColumnInfo(name = "original_title")
+    val original_title: String = "",
+    @ColumnInfo(name = "overview")
+    val overview: String = "",
+    @ColumnInfo(name = "popularity")
+    val popularity: Double = -1.0,
+    @ColumnInfo(name = "poster_path")
+    val poster_path: String = "",
+    @ColumnInfo(name = "release_date")
+    val release_date: String = "",
+    @ColumnInfo(name = "title")
+    val title: String = "",
+    @ColumnInfo(name = "video")
+    val video: Boolean = false,
+    @ColumnInfo(name = "vote_average")
+    val vote_average: Double = -1.0,
+    @ColumnInfo(name = "vote_count")
+    val vote_count: Int = -1
+)
+
+data class MovieList(val results: List<Movie> = listOf())
