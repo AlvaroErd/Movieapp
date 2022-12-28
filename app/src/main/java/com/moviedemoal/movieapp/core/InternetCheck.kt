6b708626db6d0e0afaa6205640283ead6a28f7ff
AlvaroErd <alerdoci@gmail.com>
2022-12-28 12:26:26 +0100

    App finalizada y exportada

--- app/src/main/java/com/moviedemoal/movieapp/core/InternetCheck.kt
+++ app/src/main/java/com/moviedemoal/movieapp/core/InternetCheck.kt
@@ -1,6 +1,8 @@
 package com.moviedemoal.movieapp.core
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
 import java.io.IOException
 import java.net.InetSocketAddress
 import java.net.Socket
@@ -9,7 +11,7 @@ object InternetCheck {
     suspend fun isNetworkAvailable() = coroutineScope {
         return@coroutineScope try {
             val sock = Socket()
-            val socketAddress = InetSocketAddress("8.8.8.8,", 53)
+            val socketAddress = InetSocketAddress("8.8.8.8", 53)
             sock.connect(socketAddress, 2000)
             sock.close()
             true
